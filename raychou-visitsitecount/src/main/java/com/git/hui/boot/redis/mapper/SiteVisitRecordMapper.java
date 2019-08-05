4df1de2aa232ca2a70072eb21055b730f85d97d4
RayChou <RayChou@gmail.com>
2019-08-05 11:24:58 +0800

    delete all files

--- raychou-visitsitecount/src/main/java/com/git/hui/boot/redis/mapper/SiteVisitRecordMapper.java
+++
@@ -1,8 +0,0 @@
-package com.git.hui.boot.redis.mapper;
-
-
-import com.git.hui.boot.redis.entity.SiteVisitRecord;
-import com.git.hui.boot.redis.mappers.MyMapper;
-
-public interface SiteVisitRecordMapper extends MyMapper<SiteVisitRecord> {
-}
